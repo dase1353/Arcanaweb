05d65c02637d34ff9360c4e76cea721536155d7d
陳皓軒 <dase1353@gmail.com>
2021-03-15 18:04:31 +0800

    更新

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\pacsuser\Desktop\arcanaweb\src\main.ts */"zUnb");
+module.exports = __webpack_require__(/*! C:\Users\User\Desktop\arcanaweb\src\main.ts */"zUnb");
 
 
 /***/ }),
--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\pacsuser\Desktop\arcanaweb\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\User\Desktop\arcanaweb\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
